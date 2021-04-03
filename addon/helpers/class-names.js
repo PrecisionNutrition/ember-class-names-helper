ff68a0400fbedd62a669379b0a612225fa9d9da1
Aierie <hockeu.w@gmail.com>
2021-04-03 16:47:31 +0800

    only add own properties to the object passed to classnames

--- addon/helpers/class-names.js
+++ addon/helpers/class-names.js
@@ -1,10 +1,17 @@
 import { helper } from '@ember/component/helper';
 import classnames from 'classnames';
 
+/**
+ * Params and hash are actually Proxies from glimmer:
+ *
+ * - params is a PositionalArgsProxy: https://github.com/glimmerjs/glimmer-vm/blob/09a959a14c3da4875f460ddfe545a80a7af93b04/packages/%40glimmer/manager/lib/util/args-proxy.ts#L102-L133
+ * - hash is a NamedArgsProxy: https://github.com/glimmerjs/glimmer-vm/blob/09a959a14c3da4875f460ddfe545a80a7af93b04/packages/%40glimmer/manager/lib/util/args-proxy.ts#L61-L100
+ *
+ * and hence have slightly different behaviours than arrays and conventional Javascript objects
+ */
 export default helper(function classNames(params, hash) {
-  const hashAsObject = {};
-  for (const key in hash) {
-    hashAsObject[key] = hash[key];
-  }
-  return classnames(...params, hashAsObject);
+  // convert hash to a plain Javascript object
+  const entries = Object.entries(hash);
+  hash = Object.fromEntries(entries);
+  return classnames(...params, hash);
 });
